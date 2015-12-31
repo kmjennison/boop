c735dbe5a436264c774b3eeaf2eb0de994be7870
Kevin Jennison <kevin.jennison1@gmail.com>
2015-12-31 14:41:24 -0800

    Include Facebook utils.

--- android/app/src/main/java/com/kevinjennison/boop/App.java
+++ android/app/src/main/java/com/kevinjennison/boop/App.java
@@ -1,6 +1,7 @@
 package com.kevinjennison.boop;
 import android.app.Application;
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
 import com.parse.ParseInstallation;
 
 public class App extends Application {
@@ -9,5 +10,6 @@ public class App extends Application {
         super.onCreate();
         Parse.initialize(this, "FvWH4KxfFMphTtmHuJZCxGdof0PPLh8GkiFLVyEO", "obn53G1XLAHkrHFAf0ezguSXJLuSK4SdjJKfsVY8");
         ParseInstallation.getCurrentInstallation().saveInBackground();
+        ParseFacebookUtils.initialize(this);
     }
 }
