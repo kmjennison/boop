30d1a9ad8c524069e305ba8c087ca030d05f4eb4
Kevin Jennison <kevin.jennison1@gmail.com>
2015-12-31 10:24:10 -0800

    Add Parse for push notifications.

---
+++ android/app/src/main/java/com/boop/App.java
@@ -0,0 +1,13 @@
+package com.boop;
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
+public class App extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "FvWH4KxfFMphTtmHuJZCxGdof0PPLh8GkiFLVyEO", "obn53G1XLAHkrHFAf0ezguSXJLuSK4SdjJKfsVY8");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+}
